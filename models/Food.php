d36d06a73141c1865c253eea5bbb3d15d395ee73
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-09-12 19:12:52 +0200

    creazione estensione food con condizioni di controllo

--- models/Food.php
+++ models/Food.php
@@ -11,4 +11,35 @@ class Food extends Products {
         $this->kindoffood = $kindoffood;
         $this->state =  $state;
     }
+
+
+    public function setWeight(){
+        if(isnumeric($weight) && ($weight)>0 ){
+            return $this->weight = $weight;
+        }
+    }
+    public function getWeight(){
+        return $this->weight;
+    }
+    public function setkindOfFood(){
+        if(!isnumeric($kindOfFood) && ($kindOfFood) > '' ){
+            return $this->kindOfFood = $kindOfFood;
+        }
+
+    }
+    public function getkindOfFood(){
+        return $this->kindoffood;
+    }
+    public function setState(){
+        if($state = 1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public function getState(){
+        return $this->state;
+    }
+
 }
