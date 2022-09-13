8899a9b47f969fc49e781b97a4f314aad0272c29
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-09-13 16:17:18 +0200

    modifiche sottocartella prodotto

--- models/Products/Cages.php
+++ models/Products/Cages.php
@@ -5,11 +5,39 @@ class Cages extends Products {
     $weight_range;
     $height;
 
-    public function __construct($name,$categories,$ean,$internal_code)
+    public function __construct($name,$categories,$ean,$internal_code,$weight,$weight_range,$height)
     {
         parent:: __construct($name,$categories,$ean,$internal_code);
-        $this->weight = $weight;
-        $this->weight_range = $weight_range;
-        $this->height =  $height;
+        $this->setWeight($weight);
+        $this->setWeight_Range($weight_range);
+        $this->setHeight($height);
     }
+    public function setWeight($weight){
+        if(isnumeric($weight)){
+            return $this->weight = $weight;
+        }
+
+    }
+    public function getWeight($weight){
+        return $this->weight;
+    }
+    public function setWeight_Range($weight_range){
+        if(isnumeric($weight_range)){
+            return $this->weight_range = $weight_range;
+        }
+
+    }
+    public function getWeight_Range($weight_range){
+        return $this->weight_range;
+    }
+    public function setHeight($height){
+        if(isnumeric($height)){
+            return $this->height = $height;
+        }
+
+    }
+    public function getHeight($height){
+        return $this->height;
+    }
+   
 }
--- models/Products/Games.php
+++ models/Products/Games.php
@@ -3,14 +3,39 @@ require_once __DIR__.'/Products.php';
 class Games extends Products {
     $weight;
     $kindofgames;
-    $state;
     $sound;
-    public function __construct($name,$categories,$ean,$internal_code)
+    public function __construct($name,$categories,$ean,$internal_code,$weight,$kindofgames,$sound)
     {
         parent:: __construct($name,$categories,$ean,$internal_code);
-        $this->weight = $weight;
-        $this->kindoffood = $kindofgame;
-        $this->state =  $state;
-        $this->$sound;
+        $this->setWeight($weight);
+        $this->setKindofGames($kindofgames);
+        $this->setSound($sound);
+    }
+    public function setWeight($weight){
+        if(isnumeric($weight)){
+            return $this->weight = $weight;
+        }
+
+    }
+    public function getWeight($weight){
+        return $this->weight;
+    }
+    public function setKindofGames($kindofgames){
+        if(isnumeric($kindofgames)){
+            return $this->kindofgames = $kindofgames;
+        }
+
+    }
+    public function getKindofGames($kindofgames){
+        return $this->kindofgames;
+    }
+    public function setSound($sound){
+        if(isnumeric($sound)){
+            return $this->sound = $sound;
+        }
+
+    }
+    public function getSound($sound){
+        return $this->sound;
     }
 }
